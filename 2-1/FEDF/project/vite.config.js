2942449edb5753b6237543b472eb4ab75b012996
KAMMARI-ASHRITHA376 <2410030376@klh.edu.in>
2025-11-25 21:55:12 +0530

    Update vite.config.js

--- 2-1/FEDF/project/vite.config.js
+++ 2-1/FEDF/project/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // --- ADD THIS LINE TO FIX STATIC SITE PATHS ---
-  base: './', 
+  base: ' ', 
   // --- END ADD ---
 })
